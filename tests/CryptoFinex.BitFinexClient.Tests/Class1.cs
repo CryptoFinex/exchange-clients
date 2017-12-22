d1258cd7f8397fc7f8df91ccc170ec13971d3b9f
Arkadiusz Wojcik <awojcik21@gmail.com>
2017-12-22 13:36:31 +0100

    Initial project structure

---
+++ tests/CryptoFinex.BitFinexClient.Tests/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace CryptoFinex.BitFinexClient.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Foo()
+        {
+
+        }
+
+        [Test]
+        public void Foo2()
+        {
+
+        }
+    }
+}
